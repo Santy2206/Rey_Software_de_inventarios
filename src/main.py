1eef966101a4b38e500a98b7b8b4ef6fa5b21a94
JeffersonV23 <jefers123456780@gmail.com>
2026-05-10 11:13:56 -0500

    git

--- src/main.py
+++ src/main.py
@@ -1,12 +1,99 @@
 import flet as ft
+import psycopg2
 
 
 def main(page: ft.Page):
 
-    page.title = "👑Rey"
-    page.add(ft.Text("Hello, World!"))
+    #------------------Configuración de ventana---------------------
+
+    page.title = "REY Inventarios"
+
+    page.window_width = 1200
+    page.window_height = 700
+
+    page.bgcolor = "#b3001b"
+
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    #-------------------Login Usuario--------------------------------
+
+    login_card = ft.Container(
+        width=350,
+        height=420,
+        bgcolor="#c1273d",
+        border_radius=20,
+        padding=30,
+
+        content=ft.Column(
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                ft.Text("👑", size=40),
+
+                ft.Text(
+                    "REY",
+                    size=40,
+                    weight="bold",
+                    color="white",
+                ),
+
+                ft.Text(
+                    "SOFTWARE DE INVENTARIOS",
+                    size=12,
+                    weight="bold",
+                    color="white"
+                ),
+
+                ft.TextField(
+                    hint_text="Usuario",
+                    border_radius=30,
+                    filled=True,
+                    bgcolor="#f5e6d3",
+                    border_color="transparent"
+                ),
+
+                ft.TextField(
+                    hint_text="Contraseña",
+                    password=True,
+                    can_reveal_password=True,
+                    border_radius=30,
+                    filled=True,
+                    bgcolor="#f5e6d3",
+                    border_color="transparent"
+                ),
+
+                ft.Container(height=10),
+
+                ft.ElevatedButton(
+                    "INGRESAR",
+                    width=250,
+                    height=50,
+                    bgcolor="#f2c500",
+                    color="black",
+                    style=ft.ButtonStyle(
+                        shape=ft.RoundedRectangleBorder(radius=30)
+                    )
+                ),
+
+                ft.Container(height=5),
+
+                ft.Text(
+                    "v2.0 © 2026 REY Inventarios",
+                    size=10,
+                    color="#ffb3b3"
+                )
+            ]
+        )
+    )
+
+    page.add(
+        ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+            controls=[
+                login_card
+            ]
+        )
+    )
 
 
 ft.app(target=main, view=ft.AppView.WEB_BROWSER)
 
-import psycopg2
